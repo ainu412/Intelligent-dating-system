a2feb80542f8a5565946bf40d21dc45cad02f61c
ainu412 <623287603@qq.com>
2020-02-04 00:33:19 +0800

    problems of diomend inheritance waste of space, inconvenience and error

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,39 @@
-#include "Son.h"//Son里面必须带Father,故只写Son即可
+#include <iostream>
+using namespace std;
+
+//电话
+class Tel {
+public:
+	int getNum() {
+		return num;
+	}
+protected:
+	int num = 0;
+};
+//座机
+class FixedLine :public Tel {
+
+};
+//手机
+class MobilePhone :public Tel {
+
+};
+//无线座机
+class WirelessTel :public FixedLine, public MobilePhone {
+public:
+	int getNum(){
+		return FixedLine::num;
+	}
+	void setNum(int num) {
+		this->MobilePhone::num = num;
+	}
+};
 
 int main() {
-	Son s("肉肉", 21, "红烧肉", "galgame");
+	WirelessTel w;
 
-	//解决多重继承的二义性
-	//①类外:类名限定	
-	cout << s.Father::description() << endl;
-	cout << s.Mother::description() << endl;
-
-	//②儿子类内:同名方法类名限定,覆盖多选择
-	cout << s.description() << endl;
-	s.dance();
+	w.setNum(123);//号码设定给座机,而号码获得给手机
+	cout << w.getNum() << endl;//0
 
 	return 0;
 }

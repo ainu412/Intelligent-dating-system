235c32075392cc9edce2fd03c935877c4b37a007
ainu412 <623287603@qq.com>
2020-02-04 20:28:19 +0800

    write and read text file and binary file

--- test/main.cpp
+++ test/main.cpp
@@ -1,58 +1,98 @@
-//位图算法bitmap algorithm
 #include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
-#define MAX_NUMBER 4000000000//40亿
-#define BITS_PER_BYTE 8
-#define REQUIREMENT(num) (num) % 3 == 0//要求存储的数字
-typedef unsigned int int32;//存储正数位数
-typedef int int31;//存储正数位数
+void writeFile() {
+	ofstream outFile;//ifstream也可,因后面<<是输入
+	outFile.open("练习.txt");//默认重写,只有app可以尾附
 
-void load(char* dataByteP) {
-	//num_byte_p = dataByteP / 8;
-	for (int32 num = 0; num < MAX_NUMBER; num++) {
-		//将符合要求的数所在的地址存的数存为1(1代表存有这个数)
-		if (REQUIREMENT(num)) {
-			char* num_byte_p = dataByteP + num / BITS_PER_BYTE;//求数字所在字节的首地址:&12=&0+12/8
-			//通过指针对所在字节数操作,使得要求的数存的地址存的数字为1
-			*num_byte_p = *num_byte_p | (1 << num % BITS_PER_BYTE);//所在字节的第4位:12%8=4;将这一位的数字加1存入,其他位不动加0=>将1左移4位,得到0001_0000相加(位或)原数(*num_byte_p)更新指针所指内容:改变存入的自我,不变其他
+	while (1) {
+		cout << "请输入姓名[Ctrl+Z结束]:";
+		string name;
+		cin >> name;//用cin因为是由控制台输入的,通过控制台中转,输出至txt文件
+
+		//=cin.eof()
+		if (cin.fail()) {
+			break;
 		}
+		cout << "请输入年龄:";
+		int age;
+		cin >> age;
+
+		stringstream ret;
+		ret << "姓名:" << name << ",年龄:" << age << endl;
+		outFile << ret.str();
 	}
 }
 
-bool exist(int32 inputNum, char* dataByteP) {
-	char* inputNum_byte_p = dataByteP + inputNum / BITS_PER_BYTE;//测试数字所在字节12/8=1,确定所在字节位置&12=&0+12/8
-	//检验测试数字所在字节的位(12%8)是否为1(相乘0001_0000,只要1个为1则不为0,排除其他位干扰故其他位必须为0,测验位为1测试对原测验位无改变)检验不变的自我,改变干扰的其他
-	if (0 != (*inputNum_byte_p | 1 << inputNum % BITS_PER_BYTE)) {
-		return true;
+void readTextFile() {
+	ifstream inFile;
+	inFile.open("练习.txt");
+
+	while (1) {
+		string name;
+		inFile >> name;
+		if (inFile.eof()) {//=inFile.eof()
+			break;
+		}
+		cout << name << "\t";
+
+		int age;
+		inFile >> age;
+		cout << age << endl;
 	}
-	else {
-		return false;
+
+	inFile.close();
+}
+void writeBinaryFile() {
+	ofstream outBinaryFile;
+	//ActionScript Byte Code File 文件是最常用的文件类型
+	outBinaryFile.open("二进制文件.abc", ios::binary);//或.dat
+
+	while (1) {
+		string name;
+		cout << "请输入姓名[Ctrl+Z退出]:";
+		cin >> name;
+		if (cin.fail()) {
+			break;
+		}
+		outBinaryFile << name << '\t';
+		cout << "请输入年龄:";
+		int age;
+		cin >> age;
+		//outBinaryFile << age << endl;自动转为文本方式
+		outBinaryFile.write((char*)(&age), sizeof(int));//以二进制写入
 	}
+
+	outBinaryFile.close();
 }
 
+void readBinaryFile() {
+	ifstream inBinaryFile;
+	inBinaryFile.open("二进制文件.abc", ios::binary);
+	while (1) {
+		if (inBinaryFile.fail()) {//会检测紧邻的上一次是否失败,即上一次读取age:文末上一次age成功不会break,读取name失败后读取tmp失败再读取age失败,打印出来制表符和旧的age
+			break;
+		}
+		string name;//按字符长度动态存储
+		inBinaryFile >> name;
+		//if (inBinaryFile.fail()) {//会读取文末的回车符为name,然后因为没读到新的年龄,就打印旧的年龄
+			//break;
+		//}
+		char tmp;
+		//inBinaryFile >> tmp;//文本读取可跳回车符,二进制读取不能跳=>吃掉的回车符记得也要以二进制方式吃,因文本方式会跳
+		inBinaryFile.read(&tmp, sizeof(tmp));
+		int age;
+		//inBinaryFile >> age;按文本不能读取二进制数字
+		inBinaryFile.read((char*)(&age), sizeof(age));//强制类型转换的类型记得带括号
+		cout << name << '\t' << age << endl;
+	}
+	inBinaryFile.close();
+}
 int main() {
-	//根据能存储的最大数字分配动态内存(存0-MAX)
-	int32 max_bit = MAX_NUMBER;//(int 4字节*8=32位)2的32次方>40亿,因位图算法把每一个数按1个字节1个数占存储,0-0,n-n位
-	int31 max_byte = max_bit / BITS_PER_BYTE + 1;//(int 4字节*8=32位,符号占1位)2的31次方>(40亿/8+1=5亿+1):7/8+1=1
-	char* data_byte_p = (char*)malloc(max_byte);//dataByteP = &init_num=&0;内存大小以byte计
 
-	//加载数据至分配的内存(传分配的内存的首地址data_byte_p以供加载)
-	printf("数据正在加载中...\n");
-	load(data_byte_p);
-
-	//测试代码
-	//输入
-	printf("请输入要测试的数字:");
-	int32 inputNum = -1;
-	scanf_s("%ud", &inputNum, sizeof(int32));//unsigned int和int存储的位数一样多(32)
-	//输出
-	if (exist(inputNum, data_byte_p)) {
-		printf("存在!\n");
-	}
-	else {
-		printf("不存在!\n");
-	}
+	writeFile();
 
 	return 0;
 }
-//占用内存大,加载耗时25s,但测试极快!
